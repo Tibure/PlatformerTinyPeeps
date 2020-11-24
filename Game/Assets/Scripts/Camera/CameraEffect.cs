fc7f05bff58a8910acf5fe927d3ac27e8844eef5
EmBureau <emilebureau@hotmail.com>
2020-11-24 16:20:04 -0500

    Deuxième prise Shader manque l'appel

---
+++ Game/Assets/Scripts/Camera/CameraEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour
+{
+	// Start is called before the first frame update
+	public Material material;
+	public void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, material);
+	}
+}
