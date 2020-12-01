6d8eda56e9a28d3c1aa358252e316ccacabe4e3c
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-12-01 17:23:10 -0500

    button mute dans menu

--- Game/Assets/Menu/Script/Settings.cs
+++ Game/Assets/Menu/Script/Settings.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public AudioSource sound;
+    public Slider sliderVolume;
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
+    }
+
+    public void Mute(bool isMute)
+    {
+        if (isMute)
+        {
+            audioMixer.SetFloat("volume", -100);
+        }
+        else
+        {
+            SetVolume(sliderVolume.value);
+        }
     }
 
     public void SetFullScreen(bool isFullScreen)
